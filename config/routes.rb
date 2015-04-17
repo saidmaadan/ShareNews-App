0ff061dcda8ad4463633bc2e87020e82369d8da3
Said Maadan <saidmaadan@users.noreply.github.com>
2015-04-17 17:54:33 -0500

    Setting up all users authorization and authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
-Rails.application.routes.draw do
-
-  Sharenews::Application.routes.draw do
+# Rails.application.routes.draw do
+Sharenews::Application.routes.draw do
     namespace :api do
       resources :shares
-    end
-  end
-  
+    end  
 
   devise_for :users,
         :controllers =>{
           :omniauth_callbacks => "users/omniauth_callbacks"
         }
+
+  devise_scope :user do
+    get '/api/current_user' => 'users/sessions#show_current_user'
+    post '/api/check/is_user' => 'users/users#is_user', as: 'is_user'
+  end
+
+  get '/dashboard' => 'welcome#dashboard'
+  root to: 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
